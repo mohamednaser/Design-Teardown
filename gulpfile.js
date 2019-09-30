17ac5153f66f4ed49b73ccf7cb923f0fef7860e6
Mohamed Naser <nc_m.naser@hotmail.com>
2019-09-30 22:41:46 +0200

    add gulp package to the project

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ const gulp = require("gulp");
 const autoPrefixer = require("gulp-autoprefixer");
 
 gulp.task("stylee", function(){
- return gulp.src("src/css/style.css")
+ return gulp.src("src/css/*.css")
     .pipe(autoPrefixer())
     .pipe(gulp.dest("build"));
 });
 
 gulp.task('watch' , function(){
-    gulp.watch('src/css/*.css' , ['stylee']);
+  return  gulp.watch('src/css/*.css' ,gulp.series('stylee'));
 });
