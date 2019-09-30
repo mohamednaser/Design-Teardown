c058e3e6eaa4c1d957d06ca50d5f76e636706ad1
Mohamed Naser <nc_m.naser@hotmail.com>
2019-09-30 22:58:34 +0200

    make gulp prefix all css files

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,9 @@ const gulp = require("gulp");
 const autoPrefixer = require("gulp-autoprefixer");
 
 gulp.task("stylee", function(){
- return gulp.src("src/css/style.css")
+ return gulp.src("src/css/*.css")
     .pipe(autoPrefixer())
-    .pipe(gulp.dest("build"));
+    .pipe(gulp.dest("build/css"));
 });
 
 gulp.task('watch' , function(){
